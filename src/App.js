8dd00500632b39d68da35389924760f7cdbf747a
Alejandro C. Saksida <acsaksida@gmail.com>
2020-09-02 14:40:59 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Stations from './components/stations'
+import './bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    fetch('https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json')
+    .then(resInfo => resInfo.json())
+    .then((dataInfo) => {
+      
+
+      fetch('http://gbfs.urbansharing.com/oslobysykkel.no/station_status.json')
+      .then(resStatus => resStatus.json())
+      .then((dataStatus)=> {
+        let stationsData = dataInfo.data.stations
+        let stationsInfo = dataStatus.data.stations
+
+        let mix = stationsData.map((item, i) => Object.assign({}, item, stationsInfo[i]));
+        
+        this.setState({ stations: mix })
+      })
+      .catch(console.log)
+
+      
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
+      <Stations stations={this.state.stations} />
+    )
+  }
 }
 
 export default App;
