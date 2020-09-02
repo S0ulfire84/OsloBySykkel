24f59b5ad38ccc2675967cc1e8b5c5fcc3fbd690
Alejandro C. Saksida <acsaksida@gmail.com>
2020-09-02 16:52:57 +0200

    Updates

---
+++ precache-manifest.baa630192e4aa37f974b56fb71ea75fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233bb747a0c585559d7c3b408b87eae2",
+    "url": "/OsloBySykkel/index.html"
+  },
+  {
+    "revision": "e12c110bd5a975287153",
+    "url": "/OsloBySykkel/static/css/main.d8062d14.chunk.css"
+  },
+  {
+    "revision": "cca55e8e0634ff2b5146",
+    "url": "/OsloBySykkel/static/js/2.6bf0154d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OsloBySykkel/static/js/2.6bf0154d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12c110bd5a975287153",
+    "url": "/OsloBySykkel/static/js/main.88a6dcd9.chunk.js"
+  },
+  {
+    "revision": "797e585590bd28d5ce49",
+    "url": "/OsloBySykkel/static/js/runtime-main.c33ef075.js"
+  }
+]);
